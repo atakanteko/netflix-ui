72111a40c23ccf257dc98deb27eb12d514d8b3f5
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-01-04 11:27:27 +0300

    create navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,34 @@
 import siteLogo from '@/assets/images/siteLogo.svg';
-
+import search from '@/assets/images/search.svg';
+import profilePhoto from '@/assets/images/profilePhoto.png';
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
     <nav>
-        Navbar
+        <div className="nav-wrapper">
+          <div className="nav-left">
+              <img src={siteLogo} alt="logo" />
+          </div>
+          <div className="nav-right">
+            <ul className="menu">
+              <li><a href="#" className="active">Homepage</a></li>
+              <li><a href="#">Series</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">New and Popular</a></li>
+              <li><a href="#">MyList</a></li>
+            </ul>
+            <div className="profile">
+              <div className="search">
+                <img src={search} alt="searchIcon" />
+              </div>
+              <div className="profile-img">
+                <img src={profilePhoto} alt="profilePhoto" />
+              </div>
+            </div>
+          </div>
+        </div>
     </nav>
   )
 }
